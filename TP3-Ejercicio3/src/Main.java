787cc04c92a788523b9033adfa9ce78b9809d2ca
martecno <bruno@martecno.com.ar>
2021-05-20 20:55:59 -0300

    Agregado monto del auto y corregido el código

--- TP3-Ejercicio3/src/Main.java
+++ TP3-Ejercicio3/src/Main.java
@@ -11,30 +11,32 @@ public class Main {
 		int contador = 0;
 		Double porcentaje = null;
 		Double nroPatente;
+		Double valorAuto;
 		Double totalPatentes = 0.0;
 		Double montoAPagar = null;
 
 		System.out.println("Ingrese la cantidad de patentes a ingresar");
 		cantPatentes = sc.nextInt();
 
-		while (cantPatentes < 0) {
+		while (cantPatentes < 1) {
 			System.out.println("El número ingresado debe ser superior a 1. Ingrese la cantidad de patentes a ingresar");
 			cantPatentes = sc.nextInt();
 		}
 
 		for (int i = 1; i <= cantPatentes; i++) {
-			nroPatente = (Math.random() * ((3000000 - 800000) + 1)) + 500000;
+			nroPatente = (Math.random() * ((3000000 - 800000) + 1)) + 800000;
+			valorAuto = (Math.random() * ((5000000 - 1000000) + 1)) + 1000000;
 			if (nroPatente < 1000000) {
-				montoAPagar = nroPatente * 0.05;
-				System.out.println("Su patente de: $" + df.format(nroPatente)
+				montoAPagar = valorAuto * 0.05;
+				System.out.println("Su auto es de: $" + df.format(valorAuto)
 						+ " \tPagará el 5% del valor de su modelo y es de: $" + df.format(montoAPagar));
 			} else if ((nroPatente > 1000000) && (nroPatente < 2000000)) {
-				montoAPagar = nroPatente * 0.1;
-				System.out.println("Su patente de: $" + df.format(nroPatente)
+				montoAPagar = valorAuto * 0.1;
+				System.out.println("Su auto es de: $" + df.format(valorAuto)
 						+ " \tPagará el 10% del valor de su modelo y es de: $" + df.format(montoAPagar));
 			} else if (nroPatente > 2000000) {
-				montoAPagar = nroPatente * 0.15;
-				System.out.println("Su patente de: $" + df.format(nroPatente)
+				montoAPagar = valorAuto * 0.15;
+				System.out.println("Su auto es de: $" + df.format(valorAuto)
 						+ " \tPagará el 15% del valor de su modelo y es de: $" + df.format(montoAPagar));
 
 			} else {
@@ -49,8 +51,9 @@ public class Main {
 			totalPatentes += nroPatente;
 		}
 
-		System.out.println("El monto total recaudado por el Registro Automotor es de : $" + df.format(totalPatentes));
-		System.out.println("El porcentaje de autos superiores a $1.300.000 es del: " + porcentaje + "%");
+		System.out.println(
+				"\nEl monto total recaudado por el Registro Automotor es de : $" + df.format(totalPatentes) + "\n");
+		System.out.println("El porcentaje de la numeración superior a 1.300.000 es del: " + porcentaje + "%");
 
 	}
 
